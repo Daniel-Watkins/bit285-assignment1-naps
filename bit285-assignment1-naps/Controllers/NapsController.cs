c7cab96de79ab5f1b1b59bd3e9f373c3baf3d1ac
Watkins, Daniel R <danielwatkins@student.cascadia.edu>
2019-01-29 23:54:53 -0800

    Through C2

---
+++ bit285-assignment1-naps/Controllers/NapsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bit285_assignment1_naps.Controllers
+{
+    public class NapsController : Controller
+    {
+        [HttpGet]
+        public IActionResult AccountInfo()
+        {
+            return View();
+        }
+    }
+}
